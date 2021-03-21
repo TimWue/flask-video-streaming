54ff042ca79280b146d7e49d71970c1606b7a7de
TimWue <tim.wue@gmx.de>
2021-03-21 22:38:23 +0100

    inserted my index.html (home.html) and rest of my previous work

--- app.py
+++ app.py
@@ -1,40 +1,40 @@
-#!/usr/bin/env python
-from importlib import import_module
-import os
 from flask import Flask, render_template, Response
+import logging
+from camera_pi import Camera
+#import threading
 
-# import camera driver
-if os.environ.get('CAMERA'):
-    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
-else:
-    from camera import Camera
-
-# Raspberry Pi camera module (requires picamera package)
-# from camera_pi import Camera
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    """Video streaming home page."""
-    return render_template('index.html')
-
-
+'''
+def executeStream():
+    logging.info("Started Stream")
+    while True:
+        #sleep(2)
+        my_file = open('static/stream.jpg', 'wb')
+        with picamera.PiCamera() as camera:
+            camera.capture(my_file)
+        my_file.close()
+'''
 def gen(camera):
-    """Video streaming generator function."""
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    #x = threading.Thread(target=executeStream, daemon=True)
+    #x.start()
+    app.run(host='0.0.0.0', debug=True, threaded=True)
